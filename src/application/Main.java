317311a66e65b5d8e0a2ab3ad41f62b21b110408
Shihua Lu <shihualu99@gmail.com>
2023-12-07 15:31:02 -0500

    Redesigned flow chart

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,37 +20,22 @@ public class Main {
         // Main menu
 ***REMOVED***
             System.out.println("\nChoose an option:");
-            System.out.println("1. View Products");
-            System.out.println("2. Add New Product");
 ***REMOVED***
+            System.out.println("2. Exit");
 ***REMOVED***
 
             int mainChoice = scanner.nextInt();
+    ***REMOVED***
+
             switch (mainChoice) {
 ***REMOVED***
-                    viewProducts();
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-                    System.out.println("Invalid choice. Please enter 1, 2, or 3.");
-***REMOVED***
-***REMOVED***
-***REMOVED***
-
-
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-                "LEFT JOIN brand ON product.brand_name = brand.brand_name";
-
+                    System.out.println("Invalid choice. Please enter 1 or 2.");
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
@@ -64,83 +49,165 @@ public class Main {
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-
-                System.out.println("Product Name: " + productName + ", Price: " + price + ", Size: " + size +
-                        ", URL: " + url + ", Expiration Date: " + expirationDate + ", Concern: " + concernName +
-                        ", Type: " + typeName + ", Brand: " + brandName);
-***REMOVED***
-
-***REMOVED*** catch (SQLException e) {
-***REMOVED***
-***REMOVED***
-***REMOVED***
-
-
-    private static void viewProducts() {
-        String query = "SELECT product_name, price, size, url, expiration_date FROM product";
-
-***REMOVED***
-***REMOVED***
-***REMOVED***
-
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-
-                System.out.println("Product Name: " + productName + ", Price: " + price + ", Size: " + size +
-                        ", URL: " + url + ", Expiration Date: " + expirationDate);
 ***REMOVED***
-
-            // Display sub-menu after listing products
-            displayProductsSubMenu();
-
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+                System.out.println(productCount + ". Product Name: " + productName + ", Price: " + price +
+                        ", Size: " + size + ", URL: " + url + ", Expiration Date: " + expirationDate +
+                        ", Concern: " + concernName + ", Type: " + typeName + ", Brand: " + brandName);
+***REMOVED***
 ***REMOVED*** catch (SQLException e) {
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 
 ***REMOVED***
-        System.out.println("Are you sure you want to add a new product? (yes/no):");
-        String confirmation = scanner.next();
-
-        if (!confirmation.equalsIgnoreCase("yes")) {
-            System.out.println("Add new product cancelled.");
 ***REMOVED***
 ***REMOVED***
 
-        System.out.println("Enter product name:");
-        String product_name = scanner.next();
-
-        System.out.println("Enter product price:");
+        // Prompt for product details
+***REMOVED***
 ***REMOVED***
 
-        System.out.println("Enter product size:");
+        System.out.print("Enter price: ");
+***REMOVED***
+***REMOVED***
+
+        System.out.print("Enter size: ");
         int size = scanner.nextInt();
-
-        System.out.println("Enter product url:");
-        String url = scanner.next();
-
-        System.out.println("Enter product expiration date:");
-        Long expiration = scanner.nextLong();
-
-        // Add SQL insertion logic
 ***REMOVED***
-             PreparedStatement statement = connection.prepareStatement("INSERT INTO product (product_name, price, size, url, expiration_date) VALUES (?, ?, ?, ?, ?)")) {
 
-            statement.setString(1, product_name);
+        System.out.print("Enter URL: ");
+***REMOVED***
+
+        System.out.print("Enter expiration date (YYYY-MM-DD): ");
+        String expirationDate = scanner.nextLine();
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+        System.out.print("Enter type (skincare, makeup, etc.): ");
+***REMOVED***
+        // addTypeDetails(scanner, typeName); // Optional, based on whether you need to modify ENUM values
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
 ***REMOVED***
             statement.setInt(3, size);
 ***REMOVED***
-            statement.setDate(5, new Date(expiration));
+            statement.setDate(5, java.sql.Date.valueOf(expirationDate));
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED*** else {
+***REMOVED***
+***REMOVED***
+***REMOVED*** catch (SQLException e) {
+***REMOVED***
+***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
 
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+            viewAllProducts();  // Method to display all products
+***REMOVED***
+***REMOVED***
+
+
+***REMOVED***
+        System.out.print("Enter ingredient name: ");
+***REMOVED***
+
+        System.out.print("Is the ingredient cruelty-free? (true/false): ");
+        boolean isCrueltyFree = scanner.nextBoolean();
+
+        System.out.print("Is it clean beauty? (true/false): ");
+        boolean isCleanBeauty = scanner.nextBoolean();
+
+        System.out.print("Is it fragrance-free? (true/false): ");
+        boolean isFragranceFree = scanner.nextBoolean();
+***REMOVED***
+
+***REMOVED***
+             PreparedStatement statement = connection.prepareStatement("INSERT INTO ingredient (ingredient_name, is_cruelty_free, is_clean_beauty, is_fragrance_free, product_name) VALUES (?, ?, ?, ?, ?)")) {
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED*** catch (SQLException e) {
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+        System.out.print("Enter package color: ");
+***REMOVED***
+
+        System.out.print("Enter package material: ");
+***REMOVED***
+
+        System.out.print("Enter package weight: ");
+        double weight = scanner.nextDouble();
+***REMOVED***
+
+        System.out.print("Is refill available? (true/false): ");
+        boolean refillAvailable = scanner.nextBoolean();
+
+        System.out.print("Is the package sustainable? (true/false): ");
+        boolean sustainablePackage = scanner.nextBoolean();
+***REMOVED***
+
+***REMOVED***
+             PreparedStatement statement = connection.prepareStatement("INSERT INTO package (product_name, color, material, weight, refill_available, sustainable_package) VALUES (?, ?, ?, ?, ?, ?)")) {
+
+***REMOVED***
+            statement.setString(2, color);
+            statement.setString(3, material);
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
 ***REMOVED*** catch (SQLException e) {
 ***REMOVED***
 ***REMOVED***
@@ -148,183 +215,308 @@ public class Main {
 
 
 
-***REMOVED***
-***REMOVED***
-        String keyword = scanner.nextLine();
-
-        String query = "SELECT * FROM product WHERE product_name LIKE ?"; // Assuming you're searching by name; modify as needed
 
 ***REMOVED***
+        System.out.print("Enter brand description: ");
 ***REMOVED***
 
-            statement.setString(1, "%" + keyword + "%");
-
-***REMOVED***
-    ***REMOVED***
-    ***REMOVED***
-    ***REMOVED***
-    ***REMOVED***
-                    // Add any other product attributes you need to display
-                    System.out.println("Name: " + name + ", Price: " + price+ ", Size: " + size );
-    ***REMOVED***
+        System.out.print("Enter country of origin: ");
 ***REMOVED***
 
-***REMOVED*** catch (SQLException e) {
-***REMOVED***
-***REMOVED***
-***REMOVED***
-
-    private static void filterProductsByType() {
-        System.out.println("Do you want to filter products by type? (yes/no):");
-        String confirmation = scanner.nextLine();
-
-        if (!confirmation.equalsIgnoreCase("yes")) {
-            System.out.println("Filtering by type cancelled.");
-***REMOVED***
+        System.out.print("Enter founding year: ");
+        int foundingYear = scanner.nextInt();
 ***REMOVED***
 
-        System.out.println("Enter product type to filter by:");
-        String type = scanner.nextLine();
+        System.out.print("Enter email: ");
+***REMOVED***
 
-        String query = "SELECT product_name FROM product WHERE type_name = ?";
+        System.out.print("Enter telephone number: ");
+        int tel = scanner.nextInt();
+***REMOVED***
+
+        System.out.print("Enter founder name: ");
+***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
-
-            statement.setString(1, type);
+             PreparedStatement statement = connection.prepareStatement("INSERT INTO brand (brand_name, b_description, country_of_origin, founding_year, email, tel, founder) VALUES (?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE b_description = ?, country_of_origin = ?, founding_year = ?, email = ?, tel = ?, founder = ?")) {
 
 ***REMOVED***
-                boolean productsFound = false;
-
-    ***REMOVED***
-                    productsFound = true;
-    ***REMOVED***
+            statement.setString(2, description);
+            statement.setString(3, country);
+            statement.setInt(4, foundingYear);
+            statement.setString(5, email);
+            statement.setInt(6, tel);
+            statement.setString(7, founder);
 ***REMOVED***
-    ***REMOVED***
+            statement.setString(8, description);
+            statement.setString(9, country);
+            statement.setInt(10, foundingYear);
+            statement.setString(11, email);
+            statement.setInt(12, tel);
+            statement.setString(13, founder);
 
-                if (!productsFound) {
-                    System.out.println("No products found for the specified type.");
-    ***REMOVED***
-
-***REMOVED*** catch (SQLException e) {
-    ***REMOVED***
 ***REMOVED***
 ***REMOVED*** catch (SQLException e) {
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 
-
-    private static void displayProductsSubMenu() {
-        System.out.println("\nProduct Options:");
-        System.out.println("1. Filter Products by Keyword");
-        System.out.println("2. Filter Products by Type");
-        System.out.println("3. Return to Main Menu");
+***REMOVED***
+        System.out.print("Enter concern description: ");
 ***REMOVED***
 
 ***REMOVED***
-***REMOVED*** // Consume the newline left-over
+***REMOVED***
+
+***REMOVED***
+            statement.setString(2, description);
+***REMOVED***
+
+***REMOVED***
+***REMOVED*** catch (SQLException e) {
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+
 ***REMOVED***
 ***REMOVED***
-                filterProductsByType();
 ***REMOVED***
-            case 3:
-***REMOVED*** // Return to main menu
+
 ***REMOVED***
-                System.out.println("Invalid choice. Please enter 1, 2, or 3.");
+***REMOVED***
+
+***REMOVED***
+***REMOVED*** catch (SQLException e) {
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+    private static void viewAllProducts() {
+        // SQL query to fetch and display all products
+        String query = "SELECT product_name, price, size, url, expiration_date FROM product";
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+                // Extract product details
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+                // Display product information
+                System.out.println("Name: " + name + ", Price: " + price + ", Size: " + size +
+                        ", URL: " + url + ", Expiration Date: " + expirationDate);
+***REMOVED***
+***REMOVED*** catch (SQLException e) {
+***REMOVED***
 ***REMOVED***
 ***REMOVED***
 
     private static void selectProduct(List<String> productNames) {
-        System.out.println("Enter the name of the product to select:");
-        String selectedProduct = scanner.nextLine();
+***REMOVED***
+        String selectedProductName = scanner.nextLine();
 
-        if (productNames.contains(selectedProduct)) {
-            System.out.println("You selected: " + selectedProduct);
-            // Further processing for the selected product
+        if (productNames.contains(selectedProductName)) {
+            System.out.println("You have selected: " + selectedProductName);
+
 ***REMOVED*** else {
-            System.out.println("Product not found in the list.");
-***REMOVED***
-
-
-***REMOVED***
-
-    private static void keywordSearchSubMenu(List<String> productNames) {
-        System.out.println("\nOptions:");
-        System.out.println("1. Select a product");
-        System.out.println("2. Filter these products by category");
-***REMOVED***
-
-***REMOVED***
-***REMOVED*** // Consume the newline
-
-***REMOVED***
-***REMOVED***
-                selectProduct(productNames);
-***REMOVED***
-***REMOVED***
-                filterByCategoryAndSelectProduct(productNames);
-***REMOVED***
-***REMOVED***
-                System.out.println("Invalid choice. Please enter 1 or 2.");
+            System.out.println("Product not found in the list. Please try again.");
 ***REMOVED***
 ***REMOVED***
 
 
-    private static void filterByCategoryAndSelectProduct(List<String> productNames) {
-        System.out.println("Enter category to filter by:");
-        String category = scanner.nextLine();
-        // Filter the productNames list by the specified category
-        List<String> filteredProductNames = getProductsByCategory(productNames, category);
 
-        if (filteredProductNames.isEmpty()) {
-            System.out.println("No products found in this category.");
-***REMOVED*** else {
-            // Display filtered products
-            System.out.println("Filtered Products:");
-            for (String productName : filteredProductNames) {
-                System.out.println(productName);
+
+
+
+
+
+
 ***REMOVED***
-            // Now let the user select a product
-            selectProduct(filteredProductNames);
+***REMOVED***
+***REMOVED***
+***REMOVED***
 ***REMOVED***
 ***REMOVED***
 
-    private static List<String> getProductsByCategory(List<String> productNames, String category) {
 ***REMOVED***
-        String query = "SELECT product_name FROM product WHERE type_name = ? AND product_name IN (";
-
-        // Build the query dynamically to include placeholders for each product name
-        for (int i = 0; i < productNames.size(); i++) {
-            query += "?,";
 ***REMOVED***
-        query = query.substring(0, query.length() - 1);  // Remove the trailing comma
-        query += ")";
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
 
-            statement.setString(1, category);
-            // Set each product name in the query
-            for (int i = 0; i < productNames.size(); i++) {
-                statement.setString(i + 2, productNames.get(i)); // Indexing starts at 1 and 1 is already used for category
+***REMOVED***
+***REMOVED***
 ***REMOVED***
 
+***REMOVED***
 ***REMOVED***
     ***REMOVED***
-                    filteredProductNames.add(resultSet.getString("product_name"));
+***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+                    System.out.println("Name: " + name + ", Price: " + price + ", Size: " + size +
+***REMOVED***
+***REMOVED***
+***REMOVED***
     ***REMOVED***
 ***REMOVED***
 ***REMOVED*** catch (SQLException e) {
 ***REMOVED***
 ***REMOVED***
 
-        return filteredProductNames;
+        if (filteredProductNames.isEmpty()) {
 ***REMOVED***
+***REMOVED*** else {
+            selectAndViewProduct(filteredProductNames);
+***REMOVED***
+***REMOVED***
+
+
+
+***REMOVED***
+***REMOVED***
+        if (productNames.isEmpty()) {
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED*** else {
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+    ***REMOVED*** else {
+***REMOVED***
+    ***REMOVED***
+***REMOVED***
+***REMOVED*** catch (SQLException e) {
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+
+***REMOVED***
+
+        String deleteQuery = "DELETE FROM product WHERE product_name = ?";
+***REMOVED***
+             PreparedStatement statement = connection.prepareStatement(deleteQuery)) {
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+                System.out.println("Product deleted successfully.");
+***REMOVED*** else {
+***REMOVED***
+***REMOVED***
+***REMOVED*** catch (SQLException e) {
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+    private static void editProduct(String productName) {
+
+        System.out.println("Enter new price:");
+        double newPrice = scanner.nextDouble();
+***REMOVED*** // Consume the newline
+
+        String updateQuery = "UPDATE product SET price = ? WHERE product_name = ?";
+***REMOVED***
+             PreparedStatement statement = connection.prepareStatement(updateQuery)) {
+
+            statement.setDouble(1, newPrice);
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+                System.out.println("Product updated successfully.");
+***REMOVED*** else {
+***REMOVED***
+***REMOVED***
+***REMOVED*** catch (SQLException e) {
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+
+
+
+
+
 
 
 
